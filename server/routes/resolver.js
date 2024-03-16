1fbb95c9daf565fe0683dad42d1b709ceca9df48
PrityRastogi16 <surajyoti9839@gmail.com>
2024-03-17 00:43:37 +0530

    graphql-login

--- server/routes/resolver.js
+++ server/routes/resolver.js
@@ -1,21 +1,36 @@
 const { UserModel } = require('../models/User');
 const { BookModel } = require('../models/Book');
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken");
 const resolvers = {
     Query: {
         getBooks: async () => await BookModel.find(),
-        books: async (_, { userID }) => await BookModel.find({ userID })
+        getUsers: async () => await UserModel.find()
     },
     Mutation: {
         signUpUser: async (_, {userNew }) => {
            const hashPass = await bcrypt.hash(userNew.password,5);
             const user = new UserModel({ ...userNew, password:hashPass });
-            return await user.save();
-            
+            const newUser =  await user.save();
+            return newUser;
+                  
         },
         // Add other mutation resolvers as needed
-        
+        login: async (_,{userSignIn}) => {
+            const user = await UserModel.findOne({email:userSignIn.email});
+            if(!user){
+                throw new Error("User Not Found");
+            }
+            const isPassCorrect = await bcrypt.compare(userSignIn.password, user.password);
+            if(!isPassCorrect){
+                throw new Error("Invalid Crediantials, Try again !")
+            }
+            const token = jwt.sign({userId: user.id},"prity")  
+            return {token};
+        },
     }
 };
+
+
 
 module.exports = resolvers
--- server/routes/schemaGql.js
+++ server/routes/schemaGql.js
@@ -6,24 +6,41 @@ type User {
     name: String!
     email:String!
     password:String!
+    role: String!
 }
     type Book {
         _id: ID!
         title: String!
         author: String!
+        description:String!
+    }
+    type AuthPayload {
+        token: String!
     }
  
     type Query {
-        getBooks: [Book]
-        books(userID: ID!): [Book]
-        
+        getBooks: [Book]!
+        getUsers: [User]!  
     }
 
     type Mutation {
         signUpUser(userNew: UserInput!): User
+        addBook(bookInput: BookInput): Book!
+        login(userSignIn:SignInInput!): AuthPayload
     }
+    input BookInput {
+        title: String!
+        author: String!
+        description: String!
+    }
+    
     input UserInput{
         name: String!
+         email: String!
+          password: String!
+          role:String
+    }
+    input SignInInput{
          email: String!
           password: String!
     }
